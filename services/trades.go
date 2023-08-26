f3dbf3cd29638436e27192e120f2fa8fbb9e4f2d
asolovov <andre.solovov@gmail.com>
2023-08-26 17:36:27 +0300

    update: new internal function getTrade in RetrieveTrades logic

--- services/trades.go
+++ services/trades.go
@@ -28,16 +28,6 @@ func (s *Service) RetrieveTrades(fromBlock uint64, toBLock *uint64) ([]*models.T
 		return nil, errors.GetFilterErr(err, "perps market")
 	}
 
-	trades, err := s.getTrades(iterator)
-	if err != nil {
-		return nil, err
-	}
-
-	return trades, nil
-}
-
-// getTrades is used to get models.Trade slice from given order settled events iterator
-func (s *Service) getTrades(iterator *perpsMarketGoerli.PerpsMarketGoerliOrderSettledIterator) ([]*models.Trade, error) {
 	var trades []*models.Trade
 
 	for iterator.Next() {
@@ -47,16 +37,26 @@ func (s *Service) getTrades(iterator *perpsMarketGoerli.PerpsMarketGoerliOrderSe
 		}
 		blockNumber := iterator.Event.Raw.BlockNumber
 
-		block, err := s.rpcClient.HeaderByNumber(context.Background(), big.NewInt(int64(blockNumber)))
+		trade, err := s.getTrade(iterator.Event, blockNumber)
 		if err != nil {
-			logger.Log().WithField("layer", "Service-RetrieveTrades").Errorf(
-				"get block:%v by number error: %v", blockNumber, err.Error(),
-			)
-			return nil, errors.GetFilterErr(err, "perps market")
+			return nil, err
 		}
 
-		trades = append(trades, models.GetTradeFromEvent(iterator.Event, block.Time))
+		trades = append(trades, trade)
 	}
 
 	return trades, nil
+}
+
+// getTrade is used to get models.Trade from given event and block number
+func (s *Service) getTrade(event *perpsMarketGoerli.PerpsMarketGoerliOrderSettled, blockN uint64) (*models.Trade, error) {
+	block, err := s.rpcClient.HeaderByNumber(context.Background(), big.NewInt(int64(blockN)))
+	if err != nil {
+		logger.Log().WithField("layer", "Service-RetrieveTrades").Errorf(
+			"get block:%v by number error: %v", blockN, err.Error(),
+		)
+		return nil, errors.GetFilterErr(err, "perps market")
+	}
+
+	return models.GetTradeFromEvent(event, block.Time), nil
 }
